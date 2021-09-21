b9e5b7362f228179b9cc0a253098d6ea7ab6f465
Julia <julia.juzar2@list.ru>
2021-09-21 14:46:31 +0300

    вторая

--- src/sptv20game/Sptv20Game.java
+++ src/sptv20game/Sptv20Game.java
@@ -20,18 +20,27 @@ public class Sptv20Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int max = 5 ,min = 0;
+    
+        int max = 5 ,min = 0,attempt=1;
         Random random = new Random();
         int gameNum = random.nextInt(max - min + 1)+min;
         System.out.println("Программа загадала число от 0 до 10:");
-        Scanner scanner = new Scanner(System.in);
-        int userNum = scanner.nextInt();
-        if(gameNum==userNum){
-          System.out.println("ты выйграл");
-        }else{
-          System.out.println("ты проиграл ,Я загадал:"+gameNum);
+        for(int i=0;i<3;i++){ 
+            Scanner scanner = new Scanner(System.in);
+            int userNum = scanner.nextInt();
+            if(gameNum==userNum){
+              System.out.println("ты выйграл");
+              break;
+
+            }else{
+                if(attempt < 3){
+                    System.out.println("попробуй еще разок");
+                }else{
+                   System.out.println("ты проиграл ,Я загадал:"+gameNum);
+                }
+            }
+            attempt++;// TODO code application logic here
         }
-        // TODO code application logic here
     }
+}   
     
-}
