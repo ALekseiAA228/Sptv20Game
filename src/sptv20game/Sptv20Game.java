5443c7509b73f1d610c7c38af9934757ad4b916a
Julia <julia.juzar2@list.ru>
2021-09-21 13:26:57 +0300

    first game

---
+++ src/sptv20game/Sptv20Game.java
@@ -0,0 +1,37 @@
+package sptv20game;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Sptv20Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int max = 5 ,min = 0;
+        Random random = new Random();
+        int gameNum = random.nextInt(max - min + 1)+min;
+        System.out.println("Программа загадала число от 0 до 10:");
+        Scanner scanner = new Scanner(System.in);
+        int userNum = scanner.nextInt();
+        if(gameNum==userNum){
+          System.out.println("ты выйграл");
+        }else{
+          System.out.println("ты проиграл ,Я загадал:"+gameNum);
+        }
+        // TODO code application logic here
+    }
+    
+}
